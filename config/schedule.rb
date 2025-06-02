c48762a9a083276032194d378be54a2716d0c712
Weronika Tomaszewska <wt2392@columbia.edu>
2025-06-02 15:43:36 -0400

    ASPACE-291 update README, update schedule to set up the cron job for folio_sync_prod, handle the new value in folio_sync.yml

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,13 +3,12 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
-# require File.expand_path("#{File.dirname(__FILE__)}/environment")
 require File.expand_path('../config/environment', __dir__)
 
 set :environment, Rails.env
 
 # Log cron output to app log directory
-set :output, Rails.root.join('log/test_cron_log.log').to_s
+set :output, Rails.root.join("log/#{Rails.env}_cron_log.log")
 
 set :email_subject, 'FOLIO Sync Cron Error (via Whenever Gem)'
 set :error_recipient, Rails.configuration.folio_sync[:aspace_to_folio][:developer_email_address]
@@ -17,16 +16,9 @@ set :job_template, "/usr/local/bin/mailifrc -s 'Error - :email_subject' :error_r
 
 job_type :rake, 'cd :path && :environment_variable=:environment bundle exec rake :task --silent :output'
 
-puts "Running in #{Rails.env} environment"
-if Rails.env.folio_sync_dev?
-  every 5.minutes do
-    rake 'folio_sync:aspace_to_folio:email_test instance_key=cul'
+if Rails.env.folio_sync_prod?
+  every 1.day, at: '8:00 pm' do
+    rake 'folio_sync:aspace_to_folio:run instance_key=cul modified_since=25'
+    rake 'folio_sync:aspace_to_folio:run instance_key=barnard modified_since=25'
   end
 end
-
-# if Rails.env.folio_sync_prod?
-#   every 1.day, at: '8:00 pm' do
-#     rake 'folio_sync:aspace_to_folio:run instance_key=cul modified_since=25'
-#     rake 'folio_sync:aspace_to_folio:run instance_key=barnard modified_since=25'
-#   end
-# end
