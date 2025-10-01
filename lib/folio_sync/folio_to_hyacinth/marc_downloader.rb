d7451c1a5511b61b08cf33bf46f9c693ec9979c1
Weronika Tomaszewska <wt2392@columbia.edu>
2025-10-01 15:03:30 -0400

    FOLIOSYNC-3 save SRS MARC if 965hyacinth value is found; add method for downloading single MARC based on hrid

--- lib/folio_sync/folio_to_hyacinth/marc_downloader.rb
+++ lib/folio_sync/folio_to_hyacinth/marc_downloader.rb
@@ -4,23 +4,21 @@ module FolioSync
   module FolioToHyacinth
     class MarcDownloader
       def initialize
-        # @folio_reader = FolioSync::Folio::Reader.new
         @folio_client = FolioSync::Folio::Client.instance
         @downloading_errors = []
       end
 
-      def retrieve_paginated_source_records(modified_since_utc, &block)
+      def retrieve_paginated_source_records(modified_since_utc)
         params = {
           limit: 100,
-          updatedAfter: '2025-09-26T15:35:32Z',
           offset: 0
         }
+        params[:updatedAfter] = modified_since_utc if modified_since_utc
 
         loop do
           response = @folio_client.get('source-storage/source-records', params)
 
-          puts "Retrieved another page of source records: offset=#{params[:offset]}"
-          puts "There are #{response['sourceRecords'].length} records in this page, totalRecords=#{response['totalRecords']}"
+          puts "New page: There are #{response['sourceRecords'].length} records in this page, totalRecords=#{response['totalRecords']}"
           yield(response['sourceRecords']) if block_given?
 
           break if (params[:offset] + params[:limit]) >= response['totalRecords']
@@ -34,23 +32,49 @@ module FolioSync
       def download_965hyacinth_marc_records(last_x_hours = nil)
         modified_since = Time.now.utc - (3600 * last_x_hours) if last_x_hours
         modified_since_utc = modified_since&.utc&.iso8601
-        puts "Modified since: #{modified_since_utc}"
 
         retrieve_paginated_source_records(modified_since_utc) do |source_records|
           source_records.each do |source_record|
-            puts "Source record ID is #{source_record['recordId']}"
+            if has_965hyacinth_field?(source_record)
+              puts "Found 965hyacinth field in source record ID #{source_record['parsedRecord']['content']['fields']}"
+              save_marc_record_to_file(source_record)
+            end
           end
         end
-
-        # @folio_client.get('source-storage/source-records',
-        #                   { limit: 8, updatedAfter: modified_since_utc }).each do |source_record|
-        #   puts source_record
-        # end
       end
 
-      # Downloads a single SRS MARC record to the download directory.  Raises an exception if the record with the given `folio_hrid` does NOT have at least one 965 field with a subfield $a value of '965hyacinth'.
-      # def download_single_965hyacinth_marc_record(folio_hrid)
-      # end
+      def has_965hyacinth_field?(source_record)
+        fields = source_record['parsedRecord']['content']['fields']
+
+        fields.any? do |field|
+          next unless field['965']
+
+          field['965']['subfields']&.any? { |subfield| subfield['a'] == '965hyacinth' }
+        end
+      end
+
+      def save_marc_record_to_file(source_record)
+        marc_record = source_record['parsedRecord']['content']
+        return unless marc_record
+
+        filename = "srs_record_#{source_record['recordId']}.mrc"
+        formatted_marc = MARC::Record.new_from_hash(marc_record)
+        puts "Saving formatted MARC to tmp/#{filename}"
+
+        File.binwrite("tmp/#{filename}", formatted_marc)
+      end
+
+      # Downloads a single SRS MARC record to the download directory.  Raises an exception if the record with the given `folio_hrid`
+      # does NOT have at least one 965 field with a subfield $a value of '965hyacinth'.
+      def download_single_965hyacinth_marc_record(folio_hrid)
+        source_record = @folio_client.find_source_record(instance_record_hrid: folio_hrid)
+
+        unless has_965hyacinth_field?(source_record)
+          raise "Source record with HRID #{folio_hrid} doesn't have a 965 field with subfield $a value of '965hyacinth'. Not downloading."
+        end
+
+        save_marc_record_to_file(source_record)
+      end
     end
   end
 end
--- lib/tasks/hyacinth_sync.rake
+++ lib/tasks/hyacinth_sync.rake
@@ -7,5 +7,16 @@ namespace :folio_sync do
       res = downloader.download_965hyacinth_marc_records(24)
       puts res
     end
+
+    task download_single_file: :environment do
+      FolioSync::Rake::EnvValidator.validate!(
+        ['hrid'],
+        'bundle exec rake folio_sync:folio_to_hyacinth:download_single_file hrid=123abc'
+      )
+      folio_hrid = ENV['hrid']
+
+      downloader = FolioSync::FolioToHyacinth::MarcDownloader.new
+      downloader.download_single_965hyacinth_marc_record(folio_hrid)
+    end
   end
 end
